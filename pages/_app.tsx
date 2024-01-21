05b1b32693fc8384336d3fa23e55554fe76d5f21
sbarlett <sbarlett@emeal.nttdata.com>
2024-01-21 17:16:05 -0300

    google tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,33 @@ import WrapperContainer from "@/components/layout/layout";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { DashboardProvider } from "@/store/global";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function DesktopWrapper() {
   const queryClient = new QueryClient();
+
+  const GTM_ID = "GTM-PDVW229L";
+  // const GTM_ID = "G-Q8GD0FQS88";
 
   return (
     <>
       <Head>
         <title>Challenge Puntospoint</title>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+        />
+
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GTM_ID}', {
+          page_path: window.location.pathname,
+          });
+        `}
+        </Script>
       </Head>
       <React.Fragment>
         <CssBaseline />
@@ -26,4 +45,7 @@ export default function DesktopWrapper() {
       </React.Fragment>
     </>
   );
+}
+function useReportWebVitals(arg0: (metric: any) => void) {
+  throw new Error("Function not implemented.");
 }
